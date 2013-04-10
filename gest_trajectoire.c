517e16dbff314fd7d9447942b75d260aacb08832
jraynal@enseirb-matmeca.fr <jraynal@jr-N76VM.(none)>
2013-04-10 08:41:05 +0200

    updt

--- gest_trajectoire.c
+++ gest_trajectoire.c
@@ -1,4 +1,6 @@
 #include "gest_trajectoire.h"
+
+
 static uint8_t triangle_de_vitesse( uint16_t tps ){
   uint16_t j = 0;
   uint16_t demi = (tps / 2);
@@ -17,27 +19,6 @@ static uint8_t triangle_de_vitesse( uint16_t tps ){
   }
   return 0;
 }
-
-/* static uint8_t triangle_de_vitesse_tourne( uint8_t tps ){ */
-/*   uint16 j = 0; */
-/*   uint16 demi = (tps / 2); */
-/*   /\*Accélération*\/ */
-/*   while(( RCMotG == 255 ) || (j != demi)){ */
-/*     RCMotD = (RCMotG++); */
-/*     j++; */
-/*   } */
-/*   tps = tps - demi; */
-/*   while( tps > j ) */
-/*     tps--; */
-/*   /\*Décélération*\/ */
-/*   while((RCMotG == 0) || (j != 0)){ */
-/*     RCMotD = (RCMotG--); */
-/*     j--; */
-/*   } */
-/*   return 0; */
-
-/* } */
-
 
 char avance( uint8_t cm , sens ss ){
   uint16_t tps;
@@ -74,3 +55,40 @@ char tourne( uint8_t angle , sens ss){
   triangle_de_vitesse( tps );
   return 0;
 }
+struct position_{
+  uint16_t x;
+  uint16_t y;  
+};
+
+struct etape_{
+  position p1;
+  position p2;
+  etape *suivante;
+};
+
+struct trajectoire_{
+  etape *première;
+  etape *dernière;
+};
+
+void lancer_etape( etape e ){
+
+}
+
+void creer_etape( position p1 , position p2){
+
+}
+
+void ajouter_etape( trajectoire t ){
+
+}
+
+void charger_trajectoire( void ){
+
+}
+
+static supprimer_etape( trajetoire t , etape e ){
+
+}
+
+
--- gest_trajectoire.h
+++ gest_trajectoire.h
@@ -1,10 +1,19 @@
 #ifndef _GEST_TRAJECTOIRE_H_
 #define _GEST_TRAJECTOIRE_H_
-
-#include <stdio.h>
+#include <stdint.h>
 #include "definitions.h"
+
+typedef struct etape_ etape;
+typedef struct trajectoire_ trajectoire;
+typedef struct position_ position;
 
 char avance( uint8_t ,  sens );
 char tourne( uint8_t ,  sens );
+
+void lancer_etape( etape );
+void creer_etape( position , position)
+void ajouter_etape( trajectoire );
+void charger_trajectoire( void );
+
 
 #endif /*_GEST_TRAJECTOIRE_H_*/
